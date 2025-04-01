2096347936d0030f1c6a0bd727b27eb7572d6a21
Aayush Bajaj <aayushbajaj7@gmail.com>
2025-04-02 00:48:00 +1100

    day 7

--- mastery/q51.py
+++ mastery/q51.py
@@ -4,11 +4,3 @@ list_nums = [random.randint(0,5) for _ in range(5)]
 median = lambda l: l[len(l) / 2 if len(l) % 2 == 0 else len(l)//2 ]
 print(list_nums)
 print(median(list_nums))
-
-
-
-
-
-
-
-
---
+++ mastery/q59wrong.py
@@ -0,0 +1,1 @@
+print(sorted(dic2.items(), key=lambda x:x[0])) # can sort on values with x[1]
